b93006f3da2f5fc6167168c7cbae20766b3d0b9f
danixzxc <danilagrisel@gmail.com>
2022-08-17 16:01:42 +0300

    progress bar

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,28 +4,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
+public class MainMenu : PlayerController
 {
     public bool gameOver = false;
-    public static bool gameStarted = false;
 
     public static int materialNum = 0;
 
     [SerializeField]public Animator Animator;
+
+
     public void PlayGame()
     {
         Animator.SetTrigger("GameStarted");
-        if (gameOver)
-        {
-            //Restart current scene
-            Scene scene = SceneManager.GetActiveScene();
-            SceneManager.LoadScene(scene.name);
-        }
-        else
-        {
-            //Start the game
-            gameStarted = true;
-        }
+        gameStarted = true;
+        // if (gameOver)
+        // {
+        //     //Restart current scene
+        //     Scene scene = SceneManager.GetActiveScene();
+        //     SceneManager.LoadScene(scene.name);
+        // }
+        // else
+        // {
+        //     //Start the game
+        //     gameStarted = true;
+        // }
     }
     
     public void ExitGame()
@@ -36,5 +38,18 @@ public class MainMenu : MonoBehaviour
     public void SelectMaterial(int num)
     {
         materialNum = num;
+    }
+
+    private void Update() 
+    
+    {
+        MoveProgressBar();
+    }
+
+    private void MoveProgressBar()
+    {
+        float fullLength = playerStartPosition.position.x - finishPosition.position.x;
+
+
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,25 +21,18 @@ void OnTriggerEnter(Collider trigger)
     {
         if(trigger.gameObject.tag == "Finish")
         {
-        print("GameOver!");
-        //     GroundGenerator.instance.gameOver = true;
+        Debug.Log("GameOver!");
+       // Animator.SetTrigger("GameEnded"); //and stop moving
         }
         if(trigger.gameObject.tag == "RedBonus")
         {
-            print("Decrease player!");
             transform.localScale *= 0.7f;
-
             trigger.gameObject.SetActive(false);
-
-            //     GroundGenerator.instance.gameOver = true;
         }
         if(trigger.gameObject.tag == "GreenBonus")
         {
-            print("Increase player!");
             transform.localScale *= 1.4f;
-            
             trigger.gameObject.SetActive(false);
-            //     GroundGenerator.instance.gameOver = true;
         }
     }
 
@@ -48,12 +41,18 @@ void OnTriggerEnter(Collider trigger)
     private Vector2 currentTouchPosition;
     private Vector2 endTouchPosition;
 
+
+    [SerializeField] public Transform playerStartPosition;
+    [SerializeField] public Transform finishPosition;
+
     private float moveFactorX;
 
     [SerializeField] private float maxSwerveSpeed = 1f;
     [SerializeField] private float forwardSpeed = 0.02f;
     [SerializeField]private float speed = 0.5f;
     private float swerveSpeed;
+
+    public static bool gameStarted = false;
     private void Update()
     {
         Swipe();
@@ -80,27 +79,27 @@ void OnTriggerEnter(Collider trigger)
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             startTouchPositionX = Input.GetTouch(0).position.x;
-            Debug.Log("Начало");
         }
         
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             moveFactorX = Input.GetTouch(0).position.x - startTouchPositionX;
-            startTouchPositionX = Input.GetTouch(0).position.x;            Debug.Log("Движение");
+            startTouchPositionX = Input.GetTouch(0).position.x;          
 
         }
         
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
         {
-            moveFactorX = 0f;            Debug.Log("Конец");
+            moveFactorX = 0f;           
 
         }
-
-        
         
         swerveSpeed = moveFactorX * Time.deltaTime * speed;
         swerveSpeed = Mathf.Clamp(swerveSpeed, -maxSwerveSpeed, maxSwerveSpeed);
-        transform.Translate(swerveSpeed, 0, forwardSpeed * Time.deltaTime);
+        if(gameStarted)
+            transform.Translate(swerveSpeed, 0, forwardSpeed * Time.deltaTime);
+        if(!gameStarted)
+            transform.Translate(swerveSpeed, 0, 0);
     }
     
 }
